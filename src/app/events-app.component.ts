eca96a2cce81b94310c187c3eb9b3ce9a3de6608
Mikolaj Samulak <samulak.mikolaj@outlook.com>
2022-09-18 20:54:16 +0200

    Add navigation components and few other changes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-    <h2>Hej World</h2>
-    <img src="/assets/images/basic-shield.png"/>
+    <nav-bar></nav-bar>
     <events-list></events-list>
   `
 })
