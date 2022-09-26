291c2d31dc531594812aebab76c209301afea9b9
Mikolaj Samulak <samulak.mikolaj@outlook.com>
2022-09-26 21:58:46 +0200

    Add resolver to getEvents()

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { ToastrService } from "../common/toastr.service";
 import { EventService } from "./shared/event.service";
 
@@ -8,14 +9,15 @@ import { EventService } from "./shared/event.service";
 })
 
 export class EventsListComponent {
-    events?:any[]
+    events?:any
 
-    constructor(private eventService: EventService, private toastr: ToastrService) { //dependency injection
+    constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) { //dependency injection
         
     }
 
     ngOnInit(){
-        this.events = this.eventService.getEvents()
+        //this.eventService.getEvents().subscribe(events => { this.events = events}); // subscribe
+        this.events = this.route.snapshot.data['events']; //data['events'] matches events on the routes.ts. resolver gets events from event service, route takes this and put this on the route, and we can access it in our component  
     }
 
     handleThumbnailClick(eventName: any){
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@angular/core"
+import { Subject } from "rxjs";
 
 @Injectable() //Mark as injectable is a good practice. Is required when inject a service which also inject other services.
 export class EventService {
     getEvents(){
-        return EVENTS
+      let subject = new Subject();
+      setTimeout(() => {subject.next(EVENTS); subject.complete();},100);
+      return subject;
     }
 
     getEvent(id: number){
